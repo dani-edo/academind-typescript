3a0aea6cb3b170c37c606c270e4afb6410f0e991
Dani Edo <dani.nur.afandi@gmail.com>
2020-07-18 22:40:54 +0700

    unknown and never

--- app.ts
+++ app.ts
@@ -1,26 +1,16 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 10;
+userInput = "Helooo";
+
+if (typeof userInput === "string") {
+  userName = userInput;
+  console.log(userInput, "string");
 }
 
-function printResult(num: number): void {
-  // return void type
-  console.log("Result: " + num);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (anu: number, banu: number) => number;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(5, 6));
-
-addAndHandle(10, 11, (result) => {
-  console.log("callback: ", result);
-});
+generateError("An error occured", 500);
