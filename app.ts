0cc66fa2bb75fa393bcf4ac97de3b7e09b2b5268
Dani Edo <dani.nur.afandi@gmail.com>
2020-07-18 22:16:35 +0700

    callback function

--- app.ts
+++ app.ts
@@ -7,6 +7,11 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (anu: number, banu: number) => number;
@@ -15,3 +20,7 @@ combineValues = add;
 // combineValues = 5;
 
 console.log(combineValues(5, 6));
+
+addAndHandle(10, 11, (result) => {
+  console.log("callback: ", result);
+});
